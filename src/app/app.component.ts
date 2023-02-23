7445cd6b8caf63517d681ad3865507f3eab877c7
Munzir Mukhtar Almahdi /Dev/IT/CTC/KN <momukhtar@ctcdom.ctcgroupltd.com>
2023-02-23 15:17:04 +0200

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { IPagination } from './shared/models/pagination';
+import { IProduct } from './shared/models/product';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent implements OnInit {
+
+  title = 'Skinet';
+
+  constructor() {
+
+    }
+
+  ngOnInit(): void {
+
+  }
+
+}
---
+++ src/app/shop/shop.component.ts
@@ -0,0 +1,66 @@
+import { IType } from './../shared/models/productType';
+import { IBrand } from './../shared/models/brand';
+import { ShopService } from './shop.service';
+import { IProduct } from './../shared/models/product';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.scss']
+})
+export class ShopComponent implements OnInit {
+
+  products: IProduct[];
+  brands: IBrand[];
+  types: IType[];
+  brandIdSelected: number;
+  typeIdSelected: number;
+
+  constructor(
+    private shopService: ShopService
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProducts() {
+    this.shopService.getProducts(this.brandIdSelected, this.typeIdSelected).subscribe(response => {
+      this.products = response.data
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  getBrands() {
+    this.shopService.getBrands().subscribe(response => {
+      this.brands = [{id: 0, name: 'All'}, ...response]
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  getTypes() {
+    this.shopService.getTypes().subscribe(response => {
+      this.types = [{id: 0, name: 'All'}, ...response]
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  onBrandSelected(brandId: number) {
+    this.brandIdSelected = brandId;
+    this.getProducts();
+  }
+
+  onTypeSelected(typeId: number) {
+    this.typeIdSelected = typeId;
+    this.getProducts();
+  }
+
+}
